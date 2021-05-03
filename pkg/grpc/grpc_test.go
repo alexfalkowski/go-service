18f1f7f419bff26ac07f15b90a4d815682f1c915
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-05-03 08:45:05 +0200

    test: http client with grpc gateway

--- pkg/grpc/grpc_test.go
+++ pkg/grpc/grpc_test.go
@@ -1,13 +1,18 @@
 package grpc_test
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/alexfalkowski/go-service/pkg/config"
 	pkgGRPC "github.com/alexfalkowski/go-service/pkg/grpc"
 	"github.com/alexfalkowski/go-service/pkg/grpc/internal"
+	pkgHTTP "github.com/alexfalkowski/go-service/pkg/http"
 	"github.com/alexfalkowski/go-service/pkg/logger"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.uber.org/fx"
@@ -82,10 +87,8 @@ func TestStream(t *testing.T) {
 		logger, err := logger.NewLogger(lc)
 		So(err, ShouldBeNil)
 
-		cfg := &config.Config{GRPCPort: "10006"}
-
+		cfg := &config.Config{GRPCPort: "10008"}
 		gs := pkgGRPC.NewServer(lc, &shutdowner{}, cfg, logger, pkgGRPC.NewServerOptions())
-		defer gs.GracefulStop()
 
 		internal.RegisterGreeterServer(gs, &server{})
 
@@ -113,6 +116,66 @@ func TestStream(t *testing.T) {
 
 			Convey("Then I should have a valid reply", func() {
 				So(resp.GetMessage(), ShouldEqual, "Hello test")
+			})
+
+			lc.RequireStop()
+		})
+	})
+}
+
+func TestUnaryGateway(t *testing.T) {
+	Convey("Given I have a all the servers", t, func() {
+		sh := &shutdowner{}
+		lc := fxtest.NewLifecycle(t)
+
+		logger, err := logger.NewLogger(lc)
+		So(err, ShouldBeNil)
+
+		cfg := &config.Config{GRPCPort: "10009", HTTPPort: "10010"}
+
+		mux := pkgHTTP.NewMux()
+		pkgHTTP.Register(lc, sh, mux, cfg, logger)
+
+		gs := pkgGRPC.NewServer(lc, sh, cfg, logger, pkgGRPC.NewServerOptions())
+		internal.RegisterGreeterServer(gs, &server{})
+
+		lc.RequireStart()
+
+		ctx := context.Background()
+		opts := []grpc.DialOption{grpc.WithBlock(), grpc.WithInsecure()}
+
+		conn, err := pkgGRPC.NewClient(ctx, fmt.Sprintf("127.0.0.1:%s", cfg.GRPCPort), logger, opts...)
+		So(err, ShouldBeNil)
+
+		defer conn.Close()
+
+		err = internal.RegisterGreeterHandler(ctx, mux, conn)
+		So(err, ShouldBeNil)
+
+		Convey("When I query for a greet", func() {
+			client := &http.Client{Transport: pkgHTTP.NewRoundTripper(logger)}
+
+			message := []byte(`{"name":"test"}`)
+			req, err := http.NewRequestWithContext(ctx, "POST", "http://localhost:10010/v1/greet/hello", bytes.NewBuffer(message))
+			So(err, ShouldBeNil)
+
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Request-ID", "test")
+
+			resp, err := client.Do(req)
+			So(err, ShouldBeNil)
+
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			So(err, ShouldBeNil)
+
+			actual := strings.TrimSpace(string(body))
+
+			lc.RequireStop()
+
+			Convey("Then I should have a valid reply", func() {
+				So(actual, ShouldEqual, `{"message":"Hello test"}`)
 			})
 
 			lc.RequireStop()
