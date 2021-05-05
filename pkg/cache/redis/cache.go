e6ee4eb87623f8db2ab69d75357ffa0affbe6f88
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-05-05 12:59:18 +0200

    feat(cache): use snappy to compress

--- pkg/cache/redis/cache.go
+++ pkg/cache/redis/cache.go
@@ -6,6 +6,7 @@ import (
 	"github.com/alexfalkowski/go-service/pkg/config"
 	"github.com/go-redis/cache/v8"
 	"github.com/go-redis/redis/v8"
+	"github.com/golang/snappy"
 	"go.uber.org/fx"
 	"google.golang.org/protobuf/proto"
 )
@@ -22,10 +23,20 @@ func NewCache(lc fx.Lifecycle, cfg *config.Config) *cache.Cache {
 	cache := cache.New(&cache.Options{
 		Redis: ring,
 		Marshal: func(v interface{}) ([]byte, error) {
-			return proto.Marshal(v.(proto.Message))
+			m, err := proto.Marshal(v.(proto.Message))
+			if err != nil {
+				return nil, err
+			}
+
+			return snappy.Encode(nil, m), nil
 		},
 		Unmarshal: func(b []byte, v interface{}) error {
-			return proto.Unmarshal(b, v.(proto.Message))
+			m, err := snappy.Decode(nil, b)
+			if err != nil {
+				return err
+			}
+
+			return proto.Unmarshal(m, v.(proto.Message))
 		},
 	})
 
