28d8e948d56d87d3fb909621984971cb75cca202
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-05-07 19:30:48 +0200

    test: send message

--- pkg/transport/nsq/consumer_test.go
+++ pkg/transport/nsq/consumer_test.go
@@ -1,7 +1,9 @@
 package nsq_test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/alexfalkowski/go-service/pkg/config"
 	pkgNSQ "github.com/alexfalkowski/go-service/pkg/transport/nsq"
@@ -10,9 +12,24 @@ import (
 	"go.uber.org/fx/fxtest"
 )
 
-type handler struct{}
+type handler struct {
+	m   *nsq.Message
+	mux sync.Mutex
+}
+
+func (h *handler) Message() *nsq.Message {
+	h.mux.Lock()
+	defer h.mux.Unlock()
+
+	return h.m
+}
 
 func (h *handler) HandleMessage(m *nsq.Message) error {
+	h.mux.Lock()
+	defer h.mux.Unlock()
+
+	h.m = m
+
 	return nil
 }
 
@@ -39,6 +56,46 @@ func TestConsumer(t *testing.T) {
 
 			Convey("Then I should not have an error", func() {
 				So(err, ShouldBeNil)
+			})
+
+			lc.RequireStop()
+		})
+	})
+}
+
+func TestReceiveMessage(t *testing.T) {
+	Convey("Given I have a consumer and a producer", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		systemConfig := &config.Config{
+			NSQLookupHost: "localhost:4161",
+			NSQHost:       "localhost:4150",
+		}
+		nsqConfig := pkgNSQ.NewConfig()
+		handler := &handler{}
+		params := &pkgNSQ.ConsumerParams{
+			SystemConfig: systemConfig,
+			NSQConfig:    nsqConfig,
+			Topic:        "topic",
+			Channel:      "channel",
+			Handler:      handler,
+		}
+
+		producer, err := pkgNSQ.NewProducer(lc, systemConfig, nsqConfig)
+		So(err, ShouldBeNil)
+
+		err = pkgNSQ.RegisterConsumer(lc, params)
+		So(err, ShouldBeNil)
+
+		lc.RequireStart()
+
+		Convey("When I send a message", func() {
+			err = producer.Publish("topic", []byte("test"))
+			So(err, ShouldBeNil)
+
+			time.Sleep(1 * time.Second)
+
+			Convey("Then I should receive a message", func() {
+				So(handler.Message(), ShouldNotBeNil)
 			})
 
 			lc.RequireStop()
