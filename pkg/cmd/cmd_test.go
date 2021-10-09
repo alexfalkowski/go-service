2c73bc4f6c2ad5a37d08c27a53426fa6b13ac781
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-10-09 15:39:11 +0200

    test: remove fx logger

--- pkg/cmd/cmd_test.go
+++ pkg/cmd/cmd_test.go
@@ -37,6 +37,7 @@ func TestShutdown(t *testing.T) {
 
 		Convey("When I try to run an application that will shutdown in 5 seconds", func() {
 			opts := []fx.Option{
+				fx.NopLogger,
 				config.Module, logger.ZapModule, health.GRPCModule, health.HTTPModule, health.ServerModule,
 				cache.RedisModule, cache.RistrettoModule, security.Auth0Module, sql.PostgreSQLModule,
 				trace.DataDogOpenTracingModule, trace.JaegerOpenTracingModule,
@@ -65,6 +66,7 @@ func TestInvalidHTTP(t *testing.T) {
 
 		Convey("When I try to run an application", func() {
 			opts := []fx.Option{
+				fx.NopLogger,
 				config.Module, logger.ZapModule, transport.HTTPServerModule, transport.HTTPClientModule, transport.GRPCServerModule,
 				health.GRPCModule, health.HTTPModule, health.ServerModule, fx.Provide(registrations),
 				fx.Provide(httpObserver), fx.Provide(grpcObserver),
@@ -90,6 +92,7 @@ func TestInvalidGRPC(t *testing.T) {
 
 		Convey("When I try to run an application", func() {
 			opts := []fx.Option{
+				fx.NopLogger,
 				config.Module, logger.ZapModule, transport.HTTPServerModule, transport.HTTPClientModule, transport.GRPCServerModule,
 				health.GRPCModule, health.HTTPModule, health.ServerModule, fx.Provide(registrations),
 				fx.Provide(httpObserver), fx.Provide(grpcObserver),
