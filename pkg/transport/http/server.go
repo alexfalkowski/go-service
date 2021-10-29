3fa884fff596009989f7a40d5edccd7d5c07567d
Alejandro Falkowski <alexrfalkowski@gmail.com>
2021-10-29 10:08:04 +0200

    fix(http): pass auth to custom matcher

--- pkg/transport/http/server.go
+++ pkg/transport/http/server.go
@@ -65,7 +65,7 @@ func stopServer(ctx context.Context, server *http.Server, cfg *Config, logger *z
 
 func customMatcher(key string) (string, bool) {
 	switch key {
-	case "Request-Id":
+	case "Request-Id", "Authorization":
 		return key, true
 	case "User-Agent":
 		return "ua", true
