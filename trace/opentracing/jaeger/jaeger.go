4d91914c1d1d0821d570d64ba008b706ecef2013
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-03-29 20:20:33 +0200

    feat(trace): add logger for opentracing

--- trace/opentracing/jaeger/jaeger.go
+++ trace/opentracing/jaeger/jaeger.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/alexfalkowski/go-service/os"
+	ozap "github.com/alexfalkowski/go-service/trace/opentracing/logger/zap"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jconfig "github.com/uber/jaeger-client-go/config"
-	jlog "github.com/uber/jaeger-client-go/log"
 	jprometheus "github.com/uber/jaeger-lib/metrics/prometheus"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 const (
@@ -17,7 +18,7 @@ const (
 )
 
 // Register for jaeger.
-func Register(lc fx.Lifecycle, cfg *Config) error {
+func Register(lc fx.Lifecycle, logger *zap.Logger, cfg *Config) error {
 	name, err := os.ExecutableName()
 	if err != nil {
 		return err
@@ -36,7 +37,7 @@ func Register(lc fx.Lifecycle, cfg *Config) error {
 	}
 
 	options := []jconfig.Option{
-		jconfig.Logger(jlog.NullLogger),
+		jconfig.Logger(ozap.NewLogger(logger)),
 		jconfig.Metrics(jprometheus.New()),
 	}
 
---
+++ trace/opentracing/logger/zap/zap.go
@@ -0,0 +1,52 @@
+package zap
+
+import (
+	"fmt"
+	"strings"
+
+	"go.uber.org/zap"
+)
+
+// Logger for zap.
+type Logger struct {
+	logger *zap.Logger
+}
+
+// NewLogger for zap.
+func NewLogger(logger *zap.Logger) *Logger {
+	return &Logger{logger: logger}
+}
+
+func (l *Logger) Error(msg string) {
+	l.logger.Error(msg)
+}
+
+func (l *Logger) Infof(msg string, args ...interface{}) {
+	l.logger.Info(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Debugf(msg string, args ...interface{}) {
+	l.logger.Debug(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Log(msg string) {
+	if strings.Contains(msg, "INFO") {
+		l.logger.Info(msg)
+
+		return
+	}
+
+	if strings.Contains(msg, "WARN") {
+		l.logger.Warn(msg)
+
+		return
+	}
+
+	if strings.Contains(msg, "ERROR") {
+		l.logger.Error(msg)
+
+		return
+	}
+
+	l.logger.Debug(msg)
+}
