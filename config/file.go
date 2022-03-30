a1f93ba0b0ca17d950d225aca09ddad1d71ce881
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-03-30 15:32:36 +0200

    feat(config): write to a different location (#74)

--- config/file.go
+++ config/file.go
@@ -5,12 +5,20 @@ import (
 	"os"
 )
 
+// nolint:gofumpt
+const perm = 0600
+
 // ErrMissingConfigFile for config.
 var ErrMissingConfigFile = errors.New("missing config file")
 
 // File config location.
 func File() (string, error) {
-	configFile := os.Getenv("CONFIG_FILE")
+	return FileFromEnv("CONFIG_FILE")
+}
+
+// FileFromEnv location.
+func FileFromEnv(env string) (string, error) {
+	configFile := os.Getenv(env)
 	if configFile == "" {
 		return "", ErrMissingConfigFile
 	}
@@ -28,12 +36,12 @@ func ReadFile() ([]byte, error) {
 	return os.ReadFile(configFile)
 }
 
-// WriteFile to config location.
-func WriteFile(data []byte) error {
-	configFile, err := File()
+// WriteFileToEnv location.
+func WriteFileToEnv(env string, data []byte) error {
+	configFile, err := FileFromEnv(env)
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(configFile, data, 0)
+	return os.WriteFile(configFile, data, perm)
 }
