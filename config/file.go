1ad7c4c17621956621d973755b27a0b1cd31fc27
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-04-27 01:26:09 +0200

    refactor(config): std lib will handle empty strings

--- config/file.go
+++ config/file.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
 )
@@ -9,40 +8,22 @@ import (
 // nolint:gofumpt
 const perm = 0600
 
-// ErrMissingConfigFile for config.
-var ErrMissingConfigFile = errors.New("missing config file")
-
 // File config location.
-func File() (string, error) {
+func File() string {
 	return FileFromEnv("CONFIG_FILE")
 }
 
 // FileFromEnv location.
-func FileFromEnv(env string) (string, error) {
-	configFile := os.Getenv(env)
-	if configFile == "" {
-		return "", ErrMissingConfigFile
-	}
-
-	return configFile, nil
+func FileFromEnv(env string) string {
+	return os.Getenv(env)
 }
 
 // ReadFile from config location.
 func ReadFile() ([]byte, error) {
-	configFile, err := File()
-	if err != nil {
-		return nil, err
-	}
-
-	return os.ReadFile(filepath.Clean(configFile))
+	return os.ReadFile(filepath.Clean(File()))
 }
 
 // WriteFileToEnv location.
 func WriteFileToEnv(env string, data []byte) error {
-	configFile, err := FileFromEnv(env)
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile(filepath.Clean(configFile), data, perm)
+	return os.WriteFile(filepath.Clean(FileFromEnv(env)), data, perm)
 }
