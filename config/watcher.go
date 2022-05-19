2cbf83558fd57c4c84ac27a07936c2d636ae653a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2022-05-19 09:06:57 +0200

    feat(config): watch config changes with polling

--- config/watcher.go
+++ config/watcher.go
@@ -6,6 +6,7 @@ import (
 
 	stime "github.com/alexfalkowski/go-service/time"
 	"github.com/fsnotify/fsnotify"
+	"github.com/radovskyb/watcher"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -26,6 +27,7 @@ type WatchParams struct {
 	Shutdowner fx.Shutdowner
 	Logger     *zap.Logger
 	WaitTime   WaitTime
+	Config     Configurator
 }
 
 // Watch the configuration. If it changes terminate the application.
@@ -46,39 +48,64 @@ func Watch(params WatchParams) error {
 
 // NewWatcher of config changes.
 func NewWatcher(params WatchParams) *Watcher {
-	return &Watcher{sh: params.Shutdowner, logger: params.Logger, waitTime: time.Duration(params.WaitTime)}
+	return &Watcher{sh: params.Shutdowner, logger: params.Logger, waitTime: time.Duration(params.WaitTime), cfg: params.Config}
 }
 
 // Watcher of config changes.
 type Watcher struct {
-	watcher  *fsnotify.Watcher
+	event    *fsnotify.Watcher
+	poll     *watcher.Watcher
 	sh       fx.Shutdowner
 	logger   *zap.Logger
 	waitTime time.Duration
+	cfg      Configurator
 }
 
 // Start watching.
 func (w *Watcher) Start() error {
-	watcher, err := fsnotify.NewWatcher()
+	if !w.isOSRuntime() {
+		poll := watcher.New()
+
+		poll.FilterOps(watcher.Write)
+
+		w.poll = poll
+		go w.pollWatch()
+
+		if err := poll.Add(File()); err != nil {
+			return err
+		}
+
+		go poll.Start(time.Second)
+
+		return nil
+	}
+
+	event, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
 	}
 
-	w.watcher = watcher
-	go w.watch()
+	w.event = event
+	go w.eventWatch()
 
-	return watcher.Add(File())
+	return event.Add(File())
 }
 
 // Stop watching.
 func (w *Watcher) Stop() error {
-	return w.watcher.Close()
+	if !w.isOSRuntime() {
+		w.poll.Close()
+
+		return nil
+	}
+
+	return w.event.Close()
 }
 
-func (w *Watcher) watch() {
+func (w *Watcher) eventWatch() {
 	for {
 		select {
-		case e, ok := <-w.watcher.Events:
+		case e, ok := <-w.event.Events:
 			if !ok {
 				return
 			}
@@ -88,7 +115,7 @@ func (w *Watcher) watch() {
 
 				return
 			}
-		case err, ok := <-w.watcher.Errors:
+		case err, ok := <-w.event.Errors:
 			if !ok {
 				return
 			}
@@ -102,9 +129,26 @@ func (w *Watcher) watch() {
 	}
 }
 
+func (w *Watcher) pollWatch() {
+	for {
+		select {
+		case _ = <-w.poll.Event:
+			_ = w.shutdown()
+		case err := <-w.poll.Error:
+			w.logger.Error("watching configuration", zap.Error(err))
+		case <-w.poll.Closed:
+			return
+		}
+	}
+}
+
 func (w *Watcher) shutdown() error {
 	w.logger.Info("configuration has been modified", zap.Duration("duration", w.waitTime))
 	time.Sleep(w.waitTime)
 
 	return w.sh.Shutdown()
+}
+
+func (w *Watcher) isOSRuntime() bool {
+	return w.cfg.GetRuntime() == "os"
 }
