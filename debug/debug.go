7b46648b8444c240c0d5a7059aaed0e6e56507a9
Alejandro Falkowski <alexrfalkowski@gmail.com>
2023-11-12 10:28:48 +0100

    feat(debug): add gopsutil (#428)

--- debug/debug.go
+++ debug/debug.go
@@ -2,6 +2,7 @@ package debug
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/pprof"
@@ -9,6 +10,8 @@ import (
 	"github.com/alexfalkowski/go-service/env"
 	"github.com/alexfalkowski/go-service/time"
 	"github.com/arl/statsviz"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -33,6 +36,7 @@ func Register(lc fx.Lifecycle, env env.Environment, logger *zap.Logger) {
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.HandleFunc("/debug/psutil", psutil)
 
 	lc.Append(fx.Hook{
 		OnStart: func(context.Context) error {
@@ -58,4 +62,28 @@ func start(l *zap.Logger, s *http.Server) {
 
 func stop(ctx context.Context, l *zap.Logger, s *http.Server) {
 	l.Debug("stopping debug server", zap.Error(s.Shutdown(ctx)))
+}
+
+func psutil(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	resp := make(map[string]any)
+
+	i, _ := cpu.InfoWithContext(ctx)
+	t, _ := cpu.TimesWithContext(ctx, true)
+	resp["cpu"] = map[string]any{
+		"info":  i,
+		"times": t,
+	}
+
+	s, _ := mem.SwapMemoryWithContext(ctx)
+	v, _ := mem.VirtualMemoryWithContext(ctx)
+	resp["mem"] = map[string]any{
+		"swap":    s,
+		"virtual": v,
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	b, _ := json.Marshal(resp) //nolint:errchkjson
+	w.Write(b)
 }
---
+++ debug/debug_test.go
@@ -0,0 +1,50 @@
+package debug_test
+
+import (
+	"testing"
+
+	"github.com/alexfalkowski/go-service/debug"
+	"github.com/alexfalkowski/go-service/telemetry/metrics"
+	"github.com/alexfalkowski/go-service/test"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+	"go.uber.org/fx/fxtest"
+)
+
+func TestUDebug(t *testing.T) {
+	Convey("When I have a all the servers", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		logger := test.NewLogger(lc)
+
+		debug.Register(lc, test.Environment, logger)
+
+		cfg := test.NewTransportConfig()
+
+		m, err := metrics.NewMeter(lc, test.Environment, test.Version)
+		So(err, ShouldBeNil)
+
+		lc.RequireStart()
+
+		Convey("Then all the debug URLs are valid", func() {
+			client := test.NewHTTPClient(lc, logger, test.NewTracerConfig(), cfg, m)
+			urls := []string{
+				"http://localhost:6060/debug/statsviz",
+				"http://localhost:6060/debug/pprof/",
+				"http://localhost:6060/debug/pprof/cmdline",
+				"http://localhost:6060/debug/pprof/symbol",
+				"http://localhost:6060/debug/pprof/trace",
+				"http://localhost:6060/debug/psutil",
+			}
+
+			for _, u := range urls {
+				r, err := client.Get(u)
+				So(err, ShouldBeNil)
+
+				defer r.Body.Close()
+
+				So(r.StatusCode, ShouldEqual, 200)
+			}
+
+			lc.RequireStop()
+		})
+	})
+}
