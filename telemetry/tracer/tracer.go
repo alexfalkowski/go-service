28e6d4588b09a200af5ddb1f03b8d223bcdffc2e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-02-23 23:26:12 +0100

    feat(deps): update go.opentelemetry.io/otel v1.24.0 (#505)

--- telemetry/tracer/tracer.go
+++ telemetry/tracer/tracer.go
@@ -13,7 +13,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.23.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/noop"
 	"go.uber.org/fx"
