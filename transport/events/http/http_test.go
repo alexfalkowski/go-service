e831a1a4dab7ecb23580a973508c7b17018d6929
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-12 20:02:36 +0100

    fix(events): set http error

--- transport/events/http/http_test.go
+++ transport/events/http/http_test.go
@@ -2,6 +2,7 @@ package http_test
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/alexfalkowski/go-service/hooks"
@@ -12,12 +13,24 @@ import (
 	sh "github.com/alexfalkowski/go-service/transport/http"
 	ht "github.com/alexfalkowski/go-service/transport/http/telemetry/tracer"
 	events "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/protocol"
 	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+	h "github.com/standard-webhooks/standard-webhooks/libraries/go"
 	"go.uber.org/fx/fxtest"
 )
 
 func init() {
 	tracer.Register()
+}
+
+type delRoundTripper struct {
+	rt http.RoundTripper
+}
+
+func (r *delRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Del(h.HeaderWebhookID)
+
+	return r.rt.RoundTrip(req)
 }
 
 func TestSendReceive(t *testing.T) {
@@ -62,11 +75,68 @@ func TestSendReceive(t *testing.T) {
 			e.SetType("example.type")
 			e.SetData(events.TextPlain, "test")
 
-			c.Send(ctx, e)
+			result := c.Send(ctx, e)
 
 			Convey("Then I should receive an event", func() {
+				So(protocol.IsACK(result), ShouldBeTrue)
 				So(event, ShouldNotBeNil)
 				So(string(e.Data()), ShouldEqual, "test")
+			})
+
+			lc.RequireStop()
+		})
+	})
+}
+
+func TestSendNotReceive(t *testing.T) {
+	Convey("Given I have a http event receiver", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		logger := test.NewLogger(lc)
+		cfg := test.NewInsecureTransportConfig()
+
+		m, err := metrics.NewMeter(lc, test.Environment, test.Version)
+		So(err, ShouldBeNil)
+
+		tcfg := test.NewDefaultTracerConfig()
+		t, err := ht.NewTracer(ht.Params{Lifecycle: lc, Config: tcfg, Version: test.Version})
+		So(err, ShouldBeNil)
+
+		hs := test.NewHTTPServer(lc, logger, tcfg, cfg, m, nil)
+		gs := test.NewGRPCServer(lc, logger, tcfg, cfg, false, m, nil, nil)
+
+		test.RegisterTransport(lc, gs, hs)
+
+		h, err := hooks.New(test.NewHook())
+		So(err, ShouldBeNil)
+
+		var event *events.Event
+
+		err = eh.RegisterReceiver(context.Background(), hs.Mux, "/events", func(_ context.Context, e events.Event) { event = &e }, eh.WithReceiverHook(h))
+		So(err, ShouldBeNil)
+
+		lc.RequireStart()
+
+		Convey("When I send an event", func() {
+			rt, err := sh.NewRoundTripper(sh.WithClientLogger(logger), sh.WithClientTracer(t), sh.WithClientMetrics(m))
+			So(err, ShouldBeNil)
+
+			rt = &delRoundTripper{rt: rt}
+
+			c, err := eh.NewSender(eh.WithSenderRoundTripper(rt), eh.WithSenderHook(h))
+			So(err, ShouldBeNil)
+
+			ctx := events.ContextWithTarget(context.Background(), "http://localhost:"+cfg.HTTP.Port+"/events")
+
+			e := events.NewEvent()
+			e.SetSource("example/uri")
+			e.SetType("example.type")
+			e.SetData(events.TextPlain, "test")
+
+			result := c.Send(ctx, e)
+
+			Convey("Then I should not receive an event", func() {
+				So(protocol.IsNACK(result), ShouldBeTrue)
+				So(event, ShouldBeNil)
 			})
 
 			lc.RequireStop()
