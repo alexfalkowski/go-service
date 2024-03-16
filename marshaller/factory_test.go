bf30bef0d92dab2eb8e832e1d147565800aeefa2
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-03-16 07:13:15 +0100

    fix(marshaller): factory missing type

---
+++ marshaller/factory_test.go
@@ -0,0 +1,50 @@
+package marshaller_test
+
+import (
+	"testing"
+
+	"github.com/alexfalkowski/go-service/marshaller"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+)
+
+func TestValidFactory(t *testing.T) {
+	for _, k := range []string{"yaml", "yml", "toml"} {
+		Convey("Given I have factory", t, func() {
+			p := marshaller.FactoryParams{
+				TOML: marshaller.NewTOML(),
+				YAML: marshaller.NewYAML(),
+			}
+			f := marshaller.NewFactory(p)
+
+			Convey("When I create marshaller", func() {
+				m, err := f.Create(k)
+
+				Convey("Then I should have valid marshaller", func() {
+					So(err, ShouldBeNil)
+					So(m, ShouldNotBeNil)
+				})
+			})
+		})
+	}
+}
+
+func TestInvalidFactory(t *testing.T) {
+	for _, k := range []string{"test", "bob"} {
+		Convey("Given I have factory", t, func() {
+			p := marshaller.FactoryParams{
+				TOML: marshaller.NewTOML(),
+				YAML: marshaller.NewYAML(),
+			}
+			f := marshaller.NewFactory(p)
+
+			Convey("When I create marshaller", func() {
+				m, err := f.Create(k)
+
+				Convey("Then I should have an error", func() {
+					So(err, ShouldBeError)
+					So(m, ShouldBeNil)
+				})
+			})
+		})
+	}
+}
