9072217289e2252b02b7e649b3d14fb56514deb8
Alex Falkowski <alexrfalkowski@gmail.com>
2024-04-11 18:21:43 +0200

    feat(debug): add fgprof

--- debug/pprof.go
+++ debug/pprof.go
@@ -2,6 +2,8 @@ package debug
 
 import (
 	"net/http/pprof"
+
+	"github.com/felixge/fgprof"
 )
 
 // RegisterPprof for debug.
@@ -11,4 +13,5 @@ func RegisterPprof(server *Server) {
 	server.Mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	server.Mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	server.Mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	server.Mux.Handle("/debug/fgprof", fgprof.Handler())
 }
