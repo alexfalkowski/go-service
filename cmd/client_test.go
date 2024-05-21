7e2349a8a6890c611c5bc230afecaad01cd7fecf
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-21 16:00:07 +0200

    feat(client): add run for clients that run on start

---
+++ cmd/client_test.go
@@ -0,0 +1,32 @@
+package cmd_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alexfalkowski/go-service/cmd"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+	"go.uber.org/fx/fxtest"
+)
+
+func TestStart(t *testing.T) {
+	Convey("When I start a client command without an error", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		cmd.Start(lc, func(_ context.Context) {})
+
+		Convey("Then I should not have an error", func() {
+			lc.RequireStart()
+		})
+	})
+
+	Convey("When I start a client command with an error", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		cmd.Start(lc, func(_ context.Context) { panic("whoops") })
+
+		err := lc.Start(context.Background())
+
+		Convey("Then I should have an error", func() {
+			So(err, ShouldBeError)
+		})
+	})
+}
