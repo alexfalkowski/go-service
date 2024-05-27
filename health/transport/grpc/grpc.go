7c9d536a01b5f025e878bd20198d10dd4d07023c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-27 14:01:04 +0200

    fix(health): gRPC observer should be optional (#778)

--- health/transport/grpc/grpc.go
+++ health/transport/grpc/grpc.go
@@ -2,10 +2,22 @@ package grpc
 
 import (
 	"github.com/alexfalkowski/go-service/transport/grpc"
+	"go.uber.org/fx"
 	health "google.golang.org/grpc/health/grpc_health_v1"
 )
 
+// RegisterParams for gRPC.
+type RegisterParams struct {
+	fx.In
+
+	Server   *grpc.Server
+	Observer *Observer `optional:"true"`
+}
+
 // Register health for gRPC.
-func Register(srv *grpc.Server, ob *Observer) {
-	health.RegisterHealthServer(srv.Server(), &server{ob: ob})
+func Register(params RegisterParams) {
+	ob := params.Observer
+	if ob != nil {
+		health.RegisterHealthServer(params.Server.Server(), &server{ob: ob})
+	}
 }
