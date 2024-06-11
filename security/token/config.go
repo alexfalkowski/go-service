5a69758af2e6fe61dce142b99a7821f0d3ff9040
Alex Falkowski <alexrfalkowski@gmail.com>
2024-06-11 07:49:01 +0200

    feat(token): move token to it's own configuration

--- security/token/config.go
+++ security/token/config.go
@@ -1,12 +1,15 @@
 package token
 
+import (
+	"github.com/alexfalkowski/go-service/security/token/argon2"
+)
+
 // IsEnabled the config.
 func IsEnabled(cfg *Config) bool {
-	return cfg != nil
+	return cfg != nil && cfg.Argon2 != nil
 }
 
 // Config for token.
 type Config struct {
-	Key  string `yaml:"key,omitempty" json:"key,omitempty" toml:"key,omitempty"`
-	Hash string `yaml:"hash,omitempty" json:"hash,omitempty" toml:"hash,omitempty"`
+	Argon2 *argon2.Config `yaml:"argon2,omitempty" json:"argon2,omitempty" toml:"argon2,omitempty"`
 }
