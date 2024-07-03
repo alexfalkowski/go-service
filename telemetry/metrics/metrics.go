1cb12a2980135e0868c40074aa47d4436e429f8f
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-03 08:47:56 +0200

    feat(telemetry): upgrade go.opentelemetry.io/otel to v1.28.0

--- telemetry/metrics/metrics.go
+++ telemetry/metrics/metrics.go
@@ -11,7 +11,7 @@ import (
 	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.uber.org/fx"
 )
 
