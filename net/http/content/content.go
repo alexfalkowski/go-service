5030abb5cbdc286aa382576c662267cbd8b1884d
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-05 12:44:34 +0200

    feat(http): move to content package

---
+++ net/http/content/content.go
@@ -0,0 +1,64 @@
+package content
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/alexfalkowski/go-service/marshaller"
+	"github.com/alexfalkowski/go-service/runtime"
+	ct "github.com/elnormous/contenttype"
+)
+
+const (
+	jsonMediaType = "application/json"
+	jsonKind      = "json"
+)
+
+var (
+	// TypeKey for HTTP headers.
+	TypeKey = "Content-Type"
+
+	// ErrInvalidMarshaller for content.
+	ErrInvalidMarshaller = errors.New("invalid marshaller")
+)
+
+// NewFromRequest for content.
+func NewFromRequest(req *http.Request) *Type {
+	t, err := ct.GetMediaType(req)
+	if err != nil {
+		return &Type{Media: jsonMediaType, Kind: jsonKind}
+	}
+
+	return &Type{Media: t.String(), Kind: t.Subtype}
+}
+
+// NewFromMedia for content.
+func NewFromMedia(mediaType string) *Type {
+	t, err := ct.ParseMediaType(mediaType)
+	if err != nil {
+		return &Type{Media: jsonMediaType, Kind: jsonKind}
+	}
+
+	return &Type{Media: t.String(), Kind: t.Subtype}
+}
+
+// Type for content.
+type Type struct {
+	Media string
+	Kind  string
+}
+
+// Marshaller for type.
+func (t *Type) Marshaller(mar *marshaller.Map) marshaller.Marshaller {
+	m := mar.Get(t.Kind)
+	if m == nil {
+		runtime.Must(ErrInvalidMarshaller)
+	}
+
+	return m
+}
+
+// IsText for type.
+func (t *Type) IsText() bool {
+	return t.Kind == "plain"
+}
