60dddb38f1b92edb909cf3af9698caf7f37adc6e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-07 20:33:57 +0200

    fix(http): use keys (#870)

--- net/http/rpc/context.go
+++ net/http/rpc/context.go
@@ -7,22 +7,27 @@ import (
 
 type contextKey string
 
+var (
+	requestKey  = contextKey("request")
+	responseKey = contextKey("response")
+)
+
 // WithRequest for rpc.
 func WithRequest(ctx context.Context, r *http.Request) context.Context {
-	return context.WithValue(ctx, contextKey("request"), r)
+	return context.WithValue(ctx, requestKey, r)
 }
 
 // Request for rpc.
 func Request(ctx context.Context) *http.Request {
-	return ctx.Value(contextKey("request")).(*http.Request)
+	return ctx.Value(requestKey).(*http.Request)
 }
 
 // WithResponse for rpc.
 func WithResponse(ctx context.Context, r http.ResponseWriter) context.Context {
-	return context.WithValue(ctx, contextKey("response"), r)
+	return context.WithValue(ctx, responseKey, r)
 }
 
 // Response for rpc.
 func Response(ctx context.Context) http.ResponseWriter {
-	return ctx.Value(contextKey("response")).(http.ResponseWriter)
+	return ctx.Value(responseKey).(http.ResponseWriter)
 }
