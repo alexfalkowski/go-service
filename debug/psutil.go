663ce797b6c752b9ce1dda5f37f958997abd0278
Alex Falkowski <alexrfalkowski@gmail.com>
2024-08-29 20:35:22 +0200

    feat(http): reuse content negotiating handler

--- debug/psutil.go
+++ debug/psutil.go
@@ -1,13 +1,10 @@
 package debug
 
 import (
-	"net/http"
+	"context"
 
 	"github.com/alexfalkowski/go-service/encoding"
-	"github.com/alexfalkowski/go-service/errors"
-	nh "github.com/alexfalkowski/go-service/net/http"
 	"github.com/alexfalkowski/go-service/net/http/content"
-	"github.com/alexfalkowski/go-service/net/http/status"
 	"github.com/alexfalkowski/go-service/runtime"
 	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
@@ -16,22 +13,7 @@ import (
 // RegisterPprof for debug.
 func RegisterPsutil(srv *Server, enc *encoding.Map) {
 	mux := srv.ServeMux()
-
-	psutil := func(res http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-
-		defer func() {
-			if r := recover(); r != nil {
-				err := errors.Prefix("health", runtime.ConvertRecover(r))
-				nh.WriteError(ctx, res, err, status.Code(err))
-			}
-		}()
-
-		ct := content.NewFromRequest(req)
-		res.Header().Add(content.TypeKey, ct.Media)
-
-		e := ct.Encoder(enc)
-
+	h := content.NewHandler("debug", enc, func(ctx context.Context) any {
 		data := make(map[string]any)
 
 		i, err := cpu.InfoWithContext(ctx)
@@ -60,9 +42,8 @@ func RegisterPsutil(srv *Server, enc *encoding.Map) {
 			"virtual": vm,
 		}
 
-		err = e.Encode(res, data)
-		runtime.Must(err)
-	}
+		return data
+	})
 
-	mux.HandleFunc("/debug/psutil", psutil)
+	mux.HandleFunc("/debug/psutil", h)
 }
---
+++ net/http/content/handler.go
@@ -0,0 +1,43 @@
+package content
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/alexfalkowski/go-service/encoding"
+	"github.com/alexfalkowski/go-service/errors"
+	nh "github.com/alexfalkowski/go-service/net/http"
+	hc "github.com/alexfalkowski/go-service/net/http/context"
+	"github.com/alexfalkowski/go-service/net/http/status"
+	"github.com/alexfalkowski/go-service/runtime"
+)
+
+// Handler for content.
+type Handler func(ctx context.Context) any
+
+// NewHandler for content.
+func NewHandler(prefix string, enc *encoding.Map, handler Handler) func(res http.ResponseWriter, req *http.Request) {
+	h := func(res http.ResponseWriter, req *http.Request) {
+		ctx := req.Context()
+		ctx = hc.WithRequest(ctx, req)
+		ctx = hc.WithResponse(ctx, res)
+
+		defer func() {
+			if r := recover(); r != nil {
+				err := errors.Prefix(prefix, runtime.ConvertRecover(r))
+				nh.WriteError(ctx, res, err, status.Code(err))
+			}
+		}()
+
+		ct := NewFromRequest(req)
+		res.Header().Add(TypeKey, ct.Media)
+
+		e := ct.Encoder(enc)
+		data := handler(ctx)
+
+		err := e.Encode(res, data)
+		runtime.Must(err)
+	}
+
+	return h
+}
