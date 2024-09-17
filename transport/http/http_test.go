781f9140caa58591925344fac9a661acc7984e42
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-09-17 22:45:59 +0200

    test(http): benchmark different handlers (#1007)

--- transport/http/http_test.go
+++ transport/http/http_test.go
@@ -8,10 +8,14 @@ import (
 	"time"
 
 	"github.com/alexfalkowski/go-service/runtime"
+	"github.com/alexfalkowski/go-service/telemetry/tracer"
 	"github.com/alexfalkowski/go-service/test"
+	"github.com/alexfalkowski/go-service/transport"
+	th "github.com/alexfalkowski/go-service/transport/http"
 	tm "github.com/alexfalkowski/go-service/transport/meta"
 	. "github.com/smartystreets/goconvey/convey" //nolint:revive
 	"go.uber.org/fx/fxtest"
+	"go.uber.org/zap"
 )
 
 func init() {
@@ -57,14 +61,13 @@ func TestSecure(t *testing.T) {
 	})
 }
 
-func BenchmarkHTTP(b *testing.B) {
+func BenchmarkDefaultHTTP(b *testing.B) {
 	b.ReportAllocs()
 
 	mux := http.NewServeMux()
 	p := test.Port()
 
-	mux.HandleFunc("GET /hello", func(_ http.ResponseWriter, _ *http.Request) {
-	})
+	mux.HandleFunc("GET /hello", func(_ http.ResponseWriter, _ *http.Request) {})
 
 	server := &http.Server{
 		Handler:           mux,
@@ -91,4 +94,84 @@ func BenchmarkHTTP(b *testing.B) {
 	})
 
 	b.StopTimer()
+}
+
+func BenchmarkLogHTTP(b *testing.B) {
+	b.ReportAllocs()
+
+	mux := http.NewServeMux()
+	lc := fxtest.NewLifecycle(b)
+	logger := zap.NewNop()
+	cfg := test.NewInsecureTransportConfig()
+
+	h, err := th.NewServer(th.ServerParams{
+		Shutdowner: test.NewShutdowner(), Mux: mux,
+		Config: cfg.HTTP, Logger: logger,
+		UserAgent: test.UserAgent, Version: test.Version,
+	})
+	runtime.Must(err)
+
+	transport.Register(transport.RegisterParams{Lifecycle: lc, Servers: []transport.Server{h}})
+
+	client := &http.Client{Transport: http.DefaultTransport}
+	url := fmt.Sprintf("http://%s/hello", cfg.HTTP.Address)
+
+	lc.RequireStart()
+	b.ResetTimer()
+
+	b.Run("log", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			req, err := http.NewRequestWithContext(context.Background(), "GET", url, http.NoBody)
+			runtime.Must(err)
+
+			_, err = client.Do(req)
+			runtime.Must(err)
+		}
+	})
+
+	b.StopTimer()
+	lc.RequireStop()
+}
+
+func BenchmarkTraceHTTP(b *testing.B) {
+	b.ReportAllocs()
+
+	mux := http.NewServeMux()
+	lc := fxtest.NewLifecycle(b)
+
+	tc := test.NewOTLPTracerConfig()
+	logger := zap.NewNop()
+
+	tracer, err := tracer.NewTracer(lc, test.Environment, test.Version, test.Name, tc, logger)
+	runtime.Must(err)
+
+	cfg := test.NewInsecureTransportConfig()
+
+	h, err := th.NewServer(th.ServerParams{
+		Shutdowner: test.NewShutdowner(), Mux: mux,
+		Config: cfg.HTTP, Logger: logger, Tracer: tracer,
+		UserAgent: test.UserAgent, Version: test.Version,
+	})
+	runtime.Must(err)
+
+	transport.Register(transport.RegisterParams{Lifecycle: lc, Servers: []transport.Server{h}})
+
+	client := &http.Client{Transport: http.DefaultTransport}
+	url := fmt.Sprintf("http://%s/hello", cfg.HTTP.Address)
+
+	lc.RequireStart()
+	b.ResetTimer()
+
+	b.Run("trace", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			req, err := http.NewRequestWithContext(context.Background(), "GET", url, http.NoBody)
+			runtime.Must(err)
+
+			_, err = client.Do(req)
+			runtime.Must(err)
+		}
+	})
+
+	b.StopTimer()
+	lc.RequireStop()
 }
