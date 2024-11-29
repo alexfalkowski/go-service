d9f44d8aef2d6cc2148eadba472e2ed47e4b4554
Alex Falkowski <alexrfalkowski@gmail.com>
2024-11-29 13:42:28 +0100

    feat(rest): add missing methods
    Better to be complete

--- net/http/rest/server.go
+++ net/http/rest/server.go
@@ -1,27 +1,45 @@
 package rest
 
 import (
+	"fmt"
+
 	"github.com/alexfalkowski/go-service/net/http/content"
+	"github.com/go-resty/resty/v2"
 )
 
 // Delete for rest.
 func Delete(path string, handler content.Handler) {
-	Route("DELETE "+path, handler)
+	Route(fmt.Sprintf("%s %s", resty.MethodDelete, path), handler)
 }
 
 // Get for rest.
 func Get(path string, handler content.Handler) {
-	Route("GET "+path, handler)
+	Route(fmt.Sprintf("%s %s", resty.MethodGet, path), handler)
 }
 
 // Post for rest.
 func Post(path string, handler content.Handler) {
-	Route("POST "+path, handler)
+	Route(fmt.Sprintf("%s %s", resty.MethodPost, path), handler)
 }
 
 // Put for rest.
 func Put(path string, handler content.Handler) {
-	Route("PUT "+path, handler)
+	Route(fmt.Sprintf("%s %s", resty.MethodPut, path), handler)
+}
+
+// Patch for rest.
+func Patch(path string, handler content.Handler) {
+	Route(fmt.Sprintf("%s %s", resty.MethodPatch, path), handler)
+}
+
+// Head for rest.
+func Head(path string, handler content.Handler) {
+	Route(fmt.Sprintf("%s %s", resty.MethodHead, path), handler)
+}
+
+// Options for rest.
+func Options(path string, handler content.Handler) {
+	Route(fmt.Sprintf("%s %s", resty.MethodOptions, path), handler)
 }
 
 // Route for rest.
