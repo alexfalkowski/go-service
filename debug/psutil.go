cf0a2bc6591795083f2523a726f3f9557a755072
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-12-23 12:06:09 +0100

    feat(maps): remove any and use types (#1101)
    Better to have string types for encoding.

--- debug/psutil.go
+++ debug/psutil.go
@@ -3,17 +3,37 @@ package debug
 import (
 	"context"
 
-	"github.com/alexfalkowski/go-service/maps"
 	"github.com/alexfalkowski/go-service/net/http/content"
 	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
 )
 
+type (
+	// CPU for debug.
+	CPU struct {
+		Info  []cpu.InfoStat  `yaml:"info,omitempty" json:"info,omitempty" toml:"info,omitempty"`
+		Times []cpu.TimesStat `yaml:"times,omitempty" json:"times,omitempty" toml:"times,omitempty"`
+	}
+
+	// Mem for debug.
+	Mem struct {
+		Swap    *mem.SwapMemoryStat    `yaml:"swap,omitempty" json:"swap,omitempty" toml:"swap,omitempty"`
+		Virtual *mem.VirtualMemoryStat `yaml:"virtual,omitempty" json:"virtual,omitempty" toml:"virtual,omitempty"`
+		Devices []*mem.SwapDevice      `yaml:"devices,omitempty" json:"devices,omitempty" toml:"devices,omitempty"`
+	}
+
+	// Response for debug.
+	Response struct {
+		CPU *CPU `yaml:"cpu,omitempty" json:"cpu,omitempty" toml:"cpu,omitempty"`
+		Mem *Mem `yaml:"mem,omitempty" json:"mem,omitempty" toml:"mem,omitempty"`
+	}
+)
+
 // RegisterPprof for debug.
 func RegisterPsutil(srv *Server, cont *content.Content) {
 	mux := srv.ServeMux()
-	h := content.NewHandler(cont, "debug", func(ctx context.Context) (*maps.StringAny, error) {
-		data := maps.StringAny{}
+	h := content.NewHandler(cont, "debug", func(ctx context.Context) (*Response, error) {
+		res := &Response{}
 
 		i, err := cpu.InfoWithContext(ctx)
 		if err != nil {
@@ -25,9 +45,9 @@ func RegisterPsutil(srv *Server, cont *content.Content) {
 			return nil, err
 		}
 
-		data["cpu"] = maps.StringAny{
-			"info":  i,
-			"times": t,
+		res.CPU = &CPU{
+			Info:  i,
+			Times: t,
 		}
 
 		sm, err := mem.SwapMemoryWithContext(ctx)
@@ -45,13 +65,13 @@ func RegisterPsutil(srv *Server, cont *content.Content) {
 			return nil, err
 		}
 
-		data["mem"] = maps.StringAny{
-			"swap":    sm,
-			"devices": sd,
-			"virtual": vm,
+		res.Mem = &Mem{
+			Swap:    sm,
+			Devices: sd,
+			Virtual: vm,
 		}
 
-		return &data, nil
+		return res, nil
 	})
 
 	mux.HandleFunc("/debug/psutil", h)
