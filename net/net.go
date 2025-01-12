d7d8c0f7dd757f7cdf3f2b1043e57db04e01a683
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-12 13:01:33 +0100

    fix(net): remove unused func

--- net/net.go
+++ net/net.go
@@ -1,17 +1,10 @@
 package net
 
 import (
-	"context"
 	"net"
-	"time"
 )
 
 // Listener for net.
 func Listener(address string) (net.Listener, error) {
 	return net.Listen("tcp", address)
-}
-
-// DialContext for net.
-func DialContext(_ context.Context, network, address string) (net.Conn, error) {
-	return net.DialTimeout(network, address, time.Minute)
 }
