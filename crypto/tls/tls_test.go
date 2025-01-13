bcc7ac1186d62d56896aac9c71377a5c3cd48204
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-13 16:37:52 +0100

    test(sql): add missing

--- crypto/tls/tls_test.go
+++ crypto/tls/tls_test.go
@@ -4,19 +4,36 @@ import (
 	"testing"
 
 	"github.com/alexfalkowski/go-service/crypto/tls"
+	"github.com/alexfalkowski/go-service/test"
 	. "github.com/smartystreets/goconvey/convey" //nolint:revive
 )
 
 func TestConfig(t *testing.T) {
-	cs := []*tls.Config{nil, {}}
+	configs := []*tls.Config{nil, {}}
 
-	for _, c := range cs {
+	for _, c := range configs {
 		Convey("When I try to create with missing config", t, func() {
 			c, err := tls.NewConfig(c)
 
 			Convey("Then I should have a default TLS config", func() {
 				So(c, ShouldNotBeNil)
 				So(err, ShouldBeNil)
+			})
+		})
+	}
+
+	configs = []*tls.Config{
+		test.NewTLSConfig("certs/client-cert.pem", "secrets/none"),
+		test.NewTLSConfig("secrets/none", "certs/client-key.pem"),
+		test.NewTLSConfig("secrets/hooks", "certs/client-key.pem"),
+	}
+
+	for _, c := range configs {
+		Convey("When I try to create with missing config", t, func() {
+			_, err := tls.NewConfig(c)
+
+			Convey("Then I should have an errror", func() {
+				So(err, ShouldBeError)
 			})
 		})
 	}
