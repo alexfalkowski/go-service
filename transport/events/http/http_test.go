49c39d561317615848f301009673edf20f867884
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-15 21:58:30 +0100

    test(events): add missing

--- transport/events/http/http_test.go
+++ transport/events/http/http_test.go
@@ -19,7 +19,7 @@ import (
 	"go.uber.org/fx/fxtest"
 )
 
-func TestSendReceive(t *testing.T) {
+func TestSendReceiveWithRoundTripper(t *testing.T) {
 	Convey("Given I have a http event receiver", t, func() {
 		mux := http.NewServeMux()
 		lc := fxtest.NewLifecycle(t)
@@ -50,6 +50,56 @@ func TestSendReceive(t *testing.T) {
 			rt := sh.NewRoundTripper(sh.WithClientLogger(logger), sh.WithClientTracer(tracer), sh.WithClientMetrics(m))
 
 			c, err := eh.NewSender(h, eh.WithSenderRoundTripper(rt))
+			So(err, ShouldBeNil)
+
+			ctx := events.ContextWithTarget(context.Background(), fmt.Sprintf("http://%s/events", cfg.HTTP.Address))
+
+			e := events.NewEvent()
+			e.SetSource("example/uri")
+			e.SetType("example.type")
+
+			err = e.SetData(events.TextPlain, "test")
+			So(err, ShouldBeNil)
+
+			result := c.Send(ctx, e)
+
+			Convey("Then I should receive an event", func() {
+				So(protocol.IsACK(result), ShouldBeTrue)
+				So(event, ShouldNotBeNil)
+				So(string(e.Data()), ShouldEqual, "test")
+			})
+
+			lc.RequireStop()
+		})
+	})
+}
+
+func TestSendReceiveWithoutRoundTripper(t *testing.T) {
+	Convey("Given I have a http event receiver", t, func() {
+		mux := http.NewServeMux()
+		lc := fxtest.NewLifecycle(t)
+		logger := test.NewLogger(lc)
+		cfg := test.NewInsecureTransportConfig()
+		m := test.NewOTLPMeter(lc)
+		tc := test.NewOTLPTracerConfig()
+
+		s := &test.Server{Lifecycle: lc, Logger: logger, Tracer: tc, Transport: cfg, Meter: m, Mux: mux}
+		s.Register()
+
+		h, err := hooks.New(test.NewHook())
+		So(err, ShouldBeNil)
+
+		r := eh.NewReceiver(mux, h)
+
+		var event *events.Event
+
+		err = r.Register(context.Background(), "/events", func(_ context.Context, e events.Event) { event = &e })
+		So(err, ShouldBeNil)
+
+		lc.RequireStart()
+
+		Convey("When I send an event", func() {
+			c, err := eh.NewSender(h)
 			So(err, ShouldBeNil)
 
 			ctx := events.ContextWithTarget(context.Background(), fmt.Sprintf("http://%s/events", cfg.HTTP.Address))
