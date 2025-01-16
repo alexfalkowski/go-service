5cfa016ff5e5eb7048b23758720637cf52180171
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-01-16 07:42:05 +0100

    feat(http): handle errors in a testable way (#1179)

--- net/http/http.go
+++ net/http/http.go
@@ -2,52 +2,14 @@ package http
 
 import (
 	"context"
-	"crypto/tls"
 	"errors"
-	"net"
 	"net/http"
 
-	sn "github.com/alexfalkowski/go-service/net"
+	"github.com/alexfalkowski/go-service/meta"
 )
 
-type (
-	// Server for HTTP.
-	Server struct {
-		server   *http.Server
-		tls      *tls.Config
-		listener net.Listener
-	}
-
-	// Config for HTTP.
-	Config struct {
-		TLS     *tls.Config
-		Address string
-	}
-)
-
-// NewServer for HTTP.
-func NewServer(server *http.Server, cfg *Config) (*Server, error) {
-	srv := &Server{server: server}
-
-	if cfg == nil {
-		return srv, nil
-	}
-
-	srv.tls = cfg.TLS
-
-	l, err := sn.Listener(cfg.Address)
-	if err != nil {
-		return srv, err
-	}
-
-	srv.listener = l
-
-	return srv, nil
-}
-
-// Serve the underlying server.
-func (s *Server) Serve() error {
-	err := s.serve()
+// ServerError returns nil if the err http.ErrServerClosed.
+func ServerError(err error) error {
 	if errors.Is(err, http.ErrServerClosed) {
 		return nil
 	}
@@ -55,26 +17,10 @@ func (s *Server) Serve() error {
 	return err
 }
 
-// Shutdown the underlying server.
-func (s *Server) Shutdown(ctx context.Context) error {
-	return s.server.Shutdown(ctx)
-}
-
-func (s *Server) String() string {
-	return s.listener.Addr().String()
-}
-
-// IsEnabled for server.
-func (s *Server) IsEnabled() bool {
-	return s.listener != nil
-}
-
-func (s *Server) serve() error {
-	if s.tls != nil {
-		s.server.TLSConfig = s.tls
-
-		return s.server.ServeTLS(s.listener, "", "")
+// WriteResponse handles the error and adds it to the context with meta.WithAttribute.
+func WriteResponse(ctx context.Context, res http.ResponseWriter, b []byte) {
+	if _, err := res.Write(b); err != nil {
+		meta.WithAttribute(ctx, "httpError", meta.Error(err))
+		res.WriteHeader(http.StatusInternalServerError)
 	}
-
-	return s.server.Serve(s.listener)
 }
---
+++ net/http/http_test.go
@@ -0,0 +1,42 @@
+package http_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	nh "github.com/alexfalkowski/go-service/net/http"
+	"github.com/alexfalkowski/go-service/test"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+)
+
+func TestServerClose(t *testing.T) {
+	Convey("When we check the error for a normal server close", t, func() {
+		err := nh.ServerError(http.ErrServerClosed)
+
+		Convey("Then there should be no error", func() {
+			So(err, ShouldBeNil)
+		})
+	})
+
+	Convey("When we check the error for abnormal server close", t, func() {
+		err := nh.ServerError(test.ErrFailed)
+
+		Convey("Then there should be an error", func() {
+			So(err, ShouldBeError)
+		})
+	})
+}
+
+func TestWriteResponse(t *testing.T) {
+	Convey("When we write with a bad writer", t, func() {
+		w := &test.BadResponseWriter{}
+		ctx := context.Background()
+
+		nh.WriteResponse(ctx, w, []byte("test"))
+
+		Convey("Then we should record the error", func() {
+			So(w.Code, ShouldEqual, http.StatusInternalServerError)
+		})
+	})
+}
