203684d87a1a83f6b44ca7265a69e9a1cfdb94c2
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-30 08:41:04 +0100

    test(http): make sure auth is ignored with metrics

--- transport/http/metrics_test.go
+++ transport/http/metrics_test.go
@@ -4,9 +4,62 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/alexfalkowski/go-service/crypto/ed25519"
+	"github.com/alexfalkowski/go-service/crypto/rand"
+	"github.com/alexfalkowski/go-service/id"
 	"github.com/alexfalkowski/go-service/test"
+	"github.com/alexfalkowski/go-service/token"
 	. "github.com/smartystreets/goconvey/convey" //nolint:revive
 )
+
+func TestPrometheusAuthHTTP(t *testing.T) {
+	Convey("Given I register the metrics handler", t, func() {
+		kid, _ := token.NewKID(rand.NewGenerator(rand.NewReader()))
+		a, _ := ed25519.NewSigner(test.NewEd25519())
+		id := id.Default
+		jwt := token.NewJWT(kid, a, id)
+		pas := token.NewPaseto(a, id)
+		token := token.NewToken(test.NewToken("jwt", "secrets/jwt"), test.Name, jwt, pas)
+
+		world := test.NewWorld(t,
+			test.WithWorldTelemetry("prometheus"),
+			test.WithWorldLimiter(test.NewLimiterConfig("user-agent", "1s", 100)),
+			test.WithWorldToken(token, token),
+		)
+		world.Register()
+
+		_, err := world.OpenDatabase()
+		So(err, ShouldBeNil)
+
+		_, err = world.NewRedisCache()
+		So(err, ShouldBeNil)
+
+		world.RequireStart()
+
+		Convey("When I query metrics", func() {
+			ctx, cancel := test.Timeout()
+			defer cancel()
+
+			header := http.Header{}
+
+			res, body, err := world.ResponseWithBody(ctx, "http", world.ServerHost(), http.MethodGet, "metrics", header, http.NoBody)
+			So(err, ShouldBeNil)
+
+			Convey("Then I should have valid metrics", func() {
+				So(res.StatusCode, ShouldEqual, http.StatusOK)
+
+				So(body, ShouldContainSubstring, "go_info")
+				So(body, ShouldContainSubstring, "redis_hits_total")
+				So(body, ShouldContainSubstring, "sql_max_open_total")
+				So(body, ShouldContainSubstring, "system")
+				So(body, ShouldContainSubstring, "process")
+				So(body, ShouldContainSubstring, "runtime")
+			})
+		})
+
+		world.RequireStop()
+	})
+}
 
 func TestPrometheusInsecureHTTP(t *testing.T) {
 	Convey("Given I register the metrics handler", t, func() {
