da05762e06be22df2f58217d907d6354ee8123f6
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-09 16:57:58 +0100

    docs(comments): misspelling (#1280)

--- os/os.go
+++ os/os.go
@@ -8,12 +8,12 @@ import (
 	"strings"
 )
 
-// GetVariable by keys.
+// GetVariable by key.
 func GetVariable(key string) string {
 	return os.Getenv(key)
 }
 
-// SetVariable of valle by key.
+// SetVariable of value by key.
 func SetVariable(key, value string) error {
 	return os.Setenv(key, value)
 }
