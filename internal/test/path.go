0a997eb64d067de8577702c46bb1ae23b367e625
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-13 11:40:27 +0100

    test(test): move configs and secrets to folder (#1301)

--- internal/test/path.go
+++ internal/test/path.go
@@ -3,7 +3,6 @@ package test
 import (
 	"path/filepath"
 	"runtime"
-	"strings"
 )
 
 // Path for test.
@@ -13,11 +12,7 @@ func Path(path string) string {
 	_, b, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(b)
 
-	if strings.HasPrefix(path, "certs") {
-		return filepath.Join(dir, "../../test", path)
-	}
-
-	return filepath.Join(dir, path)
+	return filepath.Join(dir, "../../test", path)
 }
 
 // FilePath for test.
