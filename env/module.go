547d1c7a0f456d4a8d1afb63b4a03f1e02453574
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-14 18:42:10 +0100

    feat(env): use debug build info for version (#1304)
    https://pkg.go.dev/runtime/debug@go1.24.0#ReadBuildInfo

--- env/module.go
+++ env/module.go
@@ -6,6 +6,7 @@ import (
 
 // Module for fx.
 var Module = fx.Options(
+	fx.Provide(NewVersion),
 	fx.Provide(NewUserAgent),
 	fx.Provide(NewName),
 )
--- env/version.go
+++ env/version.go
@@ -1,5 +1,16 @@
 package env
 
+import (
+	"runtime/debug"
+)
+
+// NewVersion for env.
+func NewVersion() Version {
+	info, _ := debug.ReadBuildInfo()
+
+	return Version(info.Main.Version)
+}
+
 // Version of the application.
 type Version string
 
