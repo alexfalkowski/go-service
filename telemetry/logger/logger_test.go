5635cfef37b10ff52c32dba8f5fb0cd236786f69
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-21 14:59:43 +0100

    fix(telemetry): make sure we get secrets for otlp logger

---
+++ telemetry/logger/logger_test.go
@@ -0,0 +1,30 @@
+package logger_test
+
+import (
+	"testing"
+
+	"github.com/alexfalkowski/go-service/internal/test"
+	"github.com/alexfalkowski/go-service/telemetry/logger"
+	. "github.com/smartystreets/goconvey/convey" //nolint:revive
+	"go.uber.org/fx/fxtest"
+)
+
+func TestLogger(t *testing.T) {
+	Convey("Given I have an invalid file system", t, func() {
+		lc := fxtest.NewLifecycle(t)
+		cfg := test.NewOTLPLoggerConfig()
+		params := logger.Params{
+			Lifecycle: lc, Config: cfg,
+			Environment: test.Environment, Version: test.Version,
+			FileSystem: &test.ErrFS{},
+		}
+
+		Convey("When I try to get a logger", func() {
+			_, err := logger.NewLogger(params)
+
+			Convey("Then I should have an error", func() {
+				So(err, ShouldBeError)
+			})
+		})
+	})
+}
