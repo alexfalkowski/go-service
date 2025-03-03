cf3fb4d04c3d1ecbab2a4eb5e4289cd0ad9e468c
Alex Falkowski <alexrfalkowski@gmail.com>
2025-03-03 08:26:17 +0100

    refactor(http): move func to errors

--- net/http/error.go
+++ net/http/error.go
@@ -2,10 +2,20 @@ package http
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
 	"github.com/alexfalkowski/go-service/meta"
 )
+
+// ServerError returns nil if the err http.ErrServerClosed.
+func ServerError(err error) error {
+	if errors.Is(err, http.ErrServerClosed) {
+		return nil
+	}
+
+	return err
+}
 
 // WriteError for http.
 func WriteError(ctx context.Context, res http.ResponseWriter, err error, status int) {
--- net/http/http.go
+++ net/http/http.go
@@ -2,20 +2,10 @@ package http
 
 import (
 	"context"
-	"errors"
 	"net/http"
 
 	"github.com/alexfalkowski/go-service/meta"
 )
-
-// ServerError returns nil if the err http.ErrServerClosed.
-func ServerError(err error) error {
-	if errors.Is(err, http.ErrServerClosed) {
-		return nil
-	}
-
-	return err
-}
 
 // WriteResponse handles the error and adds it to the context with meta.WithAttribute.
 func WriteResponse(ctx context.Context, res http.ResponseWriter, b []byte) {
