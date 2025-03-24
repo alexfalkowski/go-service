ee2a43e69d3d9b70cfebe06afc78e8e1d2b213cc
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-03-24 08:33:28 +0100

    feat(valid): pass context (#1443)

--- valid/valid.go
+++ valid/valid.go
@@ -1,15 +1,19 @@
 package valid
 
-import "github.com/go-playground/validator/v10"
+import (
+	"context"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var validate = validator.New(validator.WithRequiredStructEnabled())
 
 // Struct for type t.
-func Struct[T any](t *T) error {
-	return validate.Struct(t)
+func Struct[T any](ctx context.Context, t *T) error {
+	return validate.StructCtx(ctx, t)
 }
 
 // Field for type t and a tag (required).
-func Field[T any](t *T, tag string) error {
-	return validate.Var(t, tag)
+func Field[T any](ctx context.Context, t *T, tag string) error {
+	return validate.VarCtx(ctx, t, tag)
 }
