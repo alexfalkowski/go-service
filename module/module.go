d41e1e5a960351c6f96e789613a23405a6c29467
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-05-28 10:33:37 +0200

    feat(module): add client/server module (#1641)

--- module/module.go
+++ module/module.go
@@ -1,27 +1,70 @@
 package module
 
 import (
+	"github.com/alexfalkowski/go-service/v2/cache"
+	"github.com/alexfalkowski/go-service/v2/cli"
 	"github.com/alexfalkowski/go-service/v2/compress"
+	"github.com/alexfalkowski/go-service/v2/config"
 	"github.com/alexfalkowski/go-service/v2/crypto"
+	"github.com/alexfalkowski/go-service/v2/database/sql"
+	"github.com/alexfalkowski/go-service/v2/debug"
 	"github.com/alexfalkowski/go-service/v2/encoding"
 	"github.com/alexfalkowski/go-service/v2/env"
+	"github.com/alexfalkowski/go-service/v2/feature"
+	"github.com/alexfalkowski/go-service/v2/health"
+	"github.com/alexfalkowski/go-service/v2/hooks"
 	"github.com/alexfalkowski/go-service/v2/id"
 	"github.com/alexfalkowski/go-service/v2/os"
 	"github.com/alexfalkowski/go-service/v2/sync"
+	"github.com/alexfalkowski/go-service/v2/telemetry"
 	"github.com/alexfalkowski/go-service/v2/time"
+	"github.com/alexfalkowski/go-service/v2/token"
+	"github.com/alexfalkowski/go-service/v2/transport"
 	"github.com/alexfalkowski/go-service/v2/types"
 	"go.uber.org/fx"
 )
 
-// Module for packages in this library.
-var Module = fx.Options(
-	os.Module,
-	env.Module,
-	compress.Module,
-	encoding.Module,
-	crypto.Module,
-	time.Module,
-	sync.Module,
-	id.Module,
-	types.Module,
+var (
+	// Module for packages in this library.
+	Module = fx.Options(
+		os.Module,
+		env.Module,
+		compress.Module,
+		encoding.Module,
+		crypto.Module,
+		time.Module,
+		sync.Module,
+		id.Module,
+		types.Module,
+	)
+
+	// Server module.
+	Server = fx.Options(
+		Module,
+		debug.Module,
+		cache.Module,
+		cli.Module,
+		config.Module,
+		feature.Module,
+		hooks.Module,
+		sql.Module,
+		telemetry.Module,
+		token.Module,
+		transport.Module,
+		health.Module,
+	)
+
+	// Client module.
+	Client = fx.Options(
+		Module,
+		cache.Module,
+		cli.Module,
+		config.Module,
+		feature.Module,
+		hooks.Module,
+		sql.Module,
+		telemetry.Module,
+		token.Module,
+		transport.Module,
+	)
 )
