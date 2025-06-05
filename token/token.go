7bd6fa5950caecfe4f1370d157eeed9e8db2eb1f
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-05 21:01:32 +0200

    fix(token): ignore missing token

--- token/token.go
+++ token/token.go
@@ -47,6 +47,10 @@ type Token struct {
 
 // Generate a token based on kind.
 func (t *Token) Generate(aud, sub string) ([]byte, error) {
+	if t == nil {
+		return nil, nil
+	}
+
 	switch {
 	case t.cfg.IsSSH():
 		token, err := t.ssh.Generate()
@@ -67,6 +71,10 @@ func (t *Token) Generate(aud, sub string) ([]byte, error) {
 
 // Verify a token based on kind.
 func (t *Token) Verify(token []byte, aud string) (string, error) {
+	if t == nil {
+		return "", nil
+	}
+
 	tkn := bytes.String(token)
 
 	var (
