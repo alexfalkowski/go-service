5da894f086e7bb0aaf15011d6ec1067292404696
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-07 22:55:25 +0200

    feat(io): remove NoopWriter

--- io/noop.go
+++
@@ -1,9 +0,0 @@
-package io
-
-// NoopWriter is a writer that does nothing.
-type NoopWriter struct{}
-
-// Write implements the io.Writer interface.
-func (nw *NoopWriter) Write(_ []byte) (int, error) {
-	return 0, nil
-}
