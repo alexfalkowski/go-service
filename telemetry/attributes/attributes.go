f84effbbefb20c3f280bd57c201d34c91546a241
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-06-07 08:47:42 +0200

    feat(attributes): add more attributes (#1687)

--- telemetry/attributes/attributes.go
+++ telemetry/attributes/attributes.go
@@ -1,6 +1,9 @@
 package attributes
 
-import "go.opentelemetry.io/otel/attribute"
+import (
+	"go.opentelemetry.io/otel/attribute"
+	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
+)
 
 // Key is an alias of attribute.Key.
 type Key = attribute.Key
@@ -8,4 +11,14 @@ type Key = attribute.Key
 // String for metrics.
 func String(key, value string) attribute.KeyValue {
 	return attribute.Key(key).String(value)
+}
+
+// Int64 for metrics.
+func Int64(key string, value int64) attribute.KeyValue {
+	return attribute.Key(key).Int64(value)
+}
+
+// DBSystem for metrics.
+func DBSystem(name string) attribute.KeyValue {
+	return semconv.DBSystemNameKey.String(name)
 }
