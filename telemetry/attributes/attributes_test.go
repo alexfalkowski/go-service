765e08d7501bd72dbac4bdc4664105cb4e7a0ec7
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-07 08:40:34 +0200

    feat(attributes): add more attributes

---
+++ telemetry/attributes/attributes_test.go
@@ -0,0 +1,16 @@
+package attributes_test
+
+import (
+	"testing"
+
+	"github.com/alexfalkowski/go-service/v2/telemetry/attributes"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAttributes(t *testing.T) {
+	Convey("Then I should have attributes", t, func() {
+		So(attributes.DBSystem("test").Value.AsString(), ShouldEqual, "test")
+		So(attributes.Int64("test", 0).Value.AsInt64(), ShouldEqual, 0)
+		So(attributes.String("test", "test").Value.AsString(), ShouldEqual, "test")
+	})
+}
