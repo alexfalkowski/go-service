8dd2a8d6c44ac58384ccd4d00b3744ef6b0335f9
Alex Falkowski <alejandro.falkowski@wolt.com>
2025-06-12 05:48:59 +0300

    feat(cli): move creating flag set to cmd (#1720)

--- cli/command.go
+++ cli/command.go
@@ -1,8 +1,30 @@
 package cli
 
-import "github.com/alexfalkowski/go-service/v2/flag"
+import (
+	"github.com/alexfalkowski/go-service/v2/di"
+	"github.com/alexfalkowski/go-service/v2/flag"
+)
+
+// NewCommand creates a new command with the given name.
+func NewCommand(name string) *Command {
+	set := flag.NewFlagSet(name)
+
+	return &Command{FlagSet: set}
+}
 
 // Command for cli.
 type Command struct {
 	*flag.FlagSet
+}
+
+func (c *Command) provide() *flag.FlagSet {
+	return c.FlagSet
+}
+
+func (c *Command) module() di.Option {
+	return di.Module(
+		di.Constructor(provide),
+		di.Constructor(c.provide),
+		di.NoLogger,
+	)
 }
