337d01428aae6bb3d154a5c6bd2a253bbbaedcb7
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-11-18 22:41:49 +0100

    refactor(logger): better names (#1967)

--- telemetry/logger/module.go
+++ telemetry/logger/module.go
@@ -5,5 +5,5 @@ import "github.com/alexfalkowski/go-service/v2/di"
 // Module for fx.
 var Module = di.Module(
 	di.Constructor(NewLogger),
-	di.Constructor(newLogger),
+	di.Constructor(convertLogger),
 )
