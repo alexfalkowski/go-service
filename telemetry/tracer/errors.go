783f3416cc9a3d865e3a98952eefdb5a2202af2b
Alex Falkowski <alexrfalkowski@gmail.com>
2026-01-22 12:40:55 +0100

    fix(tracer): prefix bug in tracer errors

--- telemetry/tracer/errors.go
+++ telemetry/tracer/errors.go
@@ -3,5 +3,5 @@ package tracer
 import "github.com/alexfalkowski/go-service/v2/errors"
 
 func prefix(err error) error {
-	return errors.Prefix("metrics", err)
+	return errors.Prefix("tracer", err)
 }
