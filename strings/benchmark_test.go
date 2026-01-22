bb454efc266e469c6cfdae903672afe026998946
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-01-22 20:56:01 +0100

    feat(strings): make it more ideomatic (#2075)

--- strings/benchmark_test.go
+++ strings/benchmark_test.go
@@ -1,29 +1,33 @@
-package bytes_test
+package strings_test
 
 import (
 	"testing"
 
-	"github.com/alexfalkowski/go-service/v2/bytes"
 	"github.com/alexfalkowski/go-service/v2/crypto/rand"
+	"github.com/alexfalkowski/go-service/v2/strings"
 	"github.com/stretchr/testify/require"
 )
 
-func BenchmarkBytes(b *testing.B) {
+func BenchmarkStrings(b *testing.B) {
 	b.ReportAllocs()
 
+	cp := func(s string) []byte {
+		return []byte(s)
+	}
+
 	gen := rand.NewGenerator(rand.NewReader())
-	bs, err := gen.GenerateBytes(1024)
+	s, err := gen.GenerateText(1024)
 	require.NoError(b, err)
 
 	b.Run("copy", func(b *testing.B) {
 		for n := 0; b.Loop(); n++ {
-			_ = string(bs)
+			_ = cp(s)
 		}
 	})
 
 	b.Run("convert", func(b *testing.B) {
 		for n := 0; b.Loop(); n++ {
-			_ = bytes.String(bs)
+			_ = strings.Bytes(s)
 		}
 	})
 }
