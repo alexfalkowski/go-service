be0468f88a93ac0f72f9eec8bafc0388121e0df9
Alex Falkowski <alexrfalkowski@gmail.com>
2026-01-24 16:28:51 +0100

    fix(compress): make sure we close for zstd

--- compress/zstd/zstd.go
+++ compress/zstd/zstd.go
@@ -10,12 +10,18 @@ func NewCompressor() *Compressor {
 // Compressor for zstd.
 type Compressor struct{}
 
+// Compress using zstd.
 func (c *Compressor) Compress(data []byte) []byte {
 	e, _ := zstd.NewWriter(nil)
+	defer e.Close()
+
 	return e.EncodeAll(data, nil)
 }
 
+// Decompress using zstd.
 func (c *Compressor) Decompress(data []byte) ([]byte, error) {
 	d, _ := zstd.NewReader(nil)
+	defer d.Close()
+
 	return d.DecodeAll(data, nil)
 }
