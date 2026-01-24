7dbf83e7c30a92837e736b0fb45972299a2b9b5a
Alex Falkowski <alexrfalkowski@gmail.com>
2026-01-24 16:25:17 +0100

    feat(compress): reuse encoder/decoder for zstd

--- compress/zstd/zstd.go
+++ compress/zstd/zstd.go
@@ -4,18 +4,25 @@ import "github.com/klauspost/compress/zstd"
 
 // NewCompressor for zstd.
 func NewCompressor() *Compressor {
-	return &Compressor{}
+	// Error is never returned.
+	e, _ := zstd.NewWriter(nil)
+	d, _ := zstd.NewReader(nil)
+
+	return &Compressor{encoder: e, decoder: d}
 }
 
 // Compressor for zstd.
-type Compressor struct{}
-
-func (c *Compressor) Compress(data []byte) []byte {
-	e, _ := zstd.NewWriter(nil)
-	return e.EncodeAll(data, nil)
+type Compressor struct {
+	encoder *zstd.Encoder
+	decoder *zstd.Decoder
 }
 
+// Compress using zstd.
+func (c *Compressor) Compress(data []byte) []byte {
+	return c.encoder.EncodeAll(data, nil)
+}
+
+// Decompress using zstd.
 func (c *Compressor) Decompress(data []byte) ([]byte, error) {
-	d, _ := zstd.NewReader(nil)
-	return d.DecodeAll(data, nil)
+	return c.decoder.DecodeAll(data, nil)
 }
