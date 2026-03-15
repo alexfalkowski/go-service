11487ccf80a5dfa3c0a5f728e6dd396dda4ced4d
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-03-15 07:27:43 +0100

    fix(crypto): use raw random bytes for cryptographic byte generation and aes nonces (#2215)

--- crypto/rand/rand_test.go
+++ crypto/rand/rand_test.go
@@ -1,6 +1,7 @@
 package rand_test
 
 import (
+	"io"
 	"testing"
 
 	"github.com/alexfalkowski/go-service/v2/crypto/rand"
@@ -20,8 +21,34 @@ func TestValidRand(t *testing.T) {
 	require.Len(t, s, 32)
 }
 
+func TestGenerateBytesUsesRawBytes(t *testing.T) {
+	gen := rand.NewGenerator(staticReader{data: []byte{0x00, 0x01, 0x7f, 0x80, 0xff}})
+
+	data, err := gen.GenerateBytes(5)
+	require.NoError(t, err)
+	require.Equal(t, []byte{0x00, 0x01, 0x7f, 0x80, 0xff}, data)
+}
+
 func TestInvalidRand(t *testing.T) {
 	gen := rand.NewGenerator(&test.ErrReaderCloser{})
-	_, err := gen.GenerateText(5)
+
+	_, err := gen.GenerateBytes(5)
 	require.Error(t, err)
+
+	gen = rand.NewGenerator(&test.ErrReaderCloser{})
+	_, err = gen.GenerateText(5)
+	require.Error(t, err)
+}
+
+type staticReader struct {
+	data []byte
+}
+
+func (s staticReader) Read(p []byte) (int, error) {
+	n := copy(p, s.data)
+	if n < len(p) {
+		return n, io.EOF
+	}
+
+	return n, nil
 }
