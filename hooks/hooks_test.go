9c96c753e70888a43a8ac4f2dab9e2566cb2f67a
Alex Falkowski <alexrfalkowski@gmail.com>
2026-03-27 21:57:12 +0100

    fix(hooks): reject empty webhook secrets before constructing standard webhooks handlers

--- hooks/hooks_test.go
+++ hooks/hooks_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/alexfalkowski/go-service/v2/crypto/rand"
 	"github.com/alexfalkowski/go-service/v2/hooks"
 	"github.com/alexfalkowski/go-service/v2/internal/test"
+	"github.com/alexfalkowski/go-service/v2/strings"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,6 +22,14 @@ func TestHooks(t *testing.T) {
 
 	_, err = hooks.NewHook(test.FS, &hooks.Config{Secret: test.FilePath("secrets/redis")})
 	require.Error(t, err)
+
+	_, err = hooks.NewHook(test.FS, &hooks.Config{Secret: ""})
+	require.ErrorIs(t, err, hooks.ErrEmptySecret)
+
+	t.Setenv("EMPTY_WEBHOOK_VALUE", "")
+	source := strings.Join(":", "env", "EMPTY_WEBHOOK_VALUE")
+	_, err = hooks.NewHook(test.FS, &hooks.Config{Secret: source})
+	require.ErrorIs(t, err, hooks.ErrEmptySecret)
 
 	h, err := hooks.NewHook(nil, nil)
 	require.NoError(t, err)
