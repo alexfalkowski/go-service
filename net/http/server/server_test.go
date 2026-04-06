85c8bc10de3ad8b3d74abad489dd249b9a49691c
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-04-06 20:11:21 +0200

    test(http): reused a shared test dial helper to replace duplicated server-start retry loops and remove the fixed gRPC health startup sleeps (#2308)

--- net/http/server/server_test.go
+++ net/http/server/server_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/alexfalkowski/go-service/v2/context"
+	"github.com/alexfalkowski/go-service/v2/internal/test"
 	"github.com/alexfalkowski/go-service/v2/net/http"
 	"github.com/alexfalkowski/go-service/v2/net/http/config"
 	server "github.com/alexfalkowski/go-service/v2/net/http/server"
@@ -22,32 +23,17 @@ func TestNewServerWithRawAddress(t *testing.T) {
 		errCh <- srv.Serve()
 	}()
 
-	resp, err := request(client, t.Context(), srv.String())
+	conn, err := test.Connect(t.Context(), srv.String())
+	require.NoError(t, err)
+	require.NoError(t, conn.Close())
+
+	req, err := http.NewRequestWithContext(t.Context(), http.MethodGet, "http://"+srv.String(), nil)
+	require.NoError(t, err)
+
+	resp, err := client.Do(req)
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
 
 	require.NoError(t, srv.Shutdown(context.Background()))
 	require.NoError(t, <-errCh)
-}
-
-func request(client *http.Client, ctx context.Context, address string) (*http.Response, error) {
-	deadline := time.Now().Add(time.Second)
-	var err error
-
-	for time.Now().Before(deadline) {
-		req, reqErr := http.NewRequestWithContext(ctx, http.MethodGet, "http://"+address, nil)
-		if reqErr != nil {
-			return nil, reqErr
-		}
-
-		resp, respErr := client.Do(req)
-		if respErr == nil {
-			return resp, nil
-		}
-
-		err = respErr
-		time.Sleep(10 * time.Millisecond)
-	}
-
-	return nil, err
 }
