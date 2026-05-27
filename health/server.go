0222af1d707ee96737dd7a3808d045ae7ac8eaa3
Alex Falkowski <alexrfalkowski@gmail.com>
2026-05-27 12:23:26 +0200

    feat(deps): update github.com/alexfalkowski/go-health/v2 to v2.23.0

--- health/server.go
+++ health/server.go
@@ -2,7 +2,6 @@ package health
 
 import (
 	health "github.com/alexfalkowski/go-health/v2/server"
-	"github.com/alexfalkowski/go-service/v2/context"
 	"github.com/alexfalkowski/go-service/v2/di"
 )
 
@@ -19,16 +18,8 @@ func NewServer(lc di.Lifecycle) *health.Server {
 	server := health.NewServer()
 
 	lc.Append(di.Hook{
-		OnStart: func(_ context.Context) error {
-			server.Start()
-
-			return nil
-		},
-		OnStop: func(_ context.Context) error {
-			server.Stop()
-
-			return nil
-		},
+		OnStart: server.Start,
+		OnStop:  server.Stop,
 	})
 
 	return server
